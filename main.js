52aabe9f9582bdfcb66f637ed1b9723367eedcb5
Jake Keller <jakekeller321@gmail.com>
2022-04-10 13:09:24 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+const express = require("express"),
+    app = express(),
+    homeController = require("./controllers/homeController"),
+    errorController = require("./controllers/errorController"),
+    layouts = require("express-ejs-layouts");
+    //mathFunc = require("./gradeFunctions")
+    //testController = require("./controllers/testController");
+
+app.set("port", process.env.PORT || 3000);
+
+// Enable EJS layout rendering
+app.use(layouts);
+app.set('layout', './layouts/full-width')
+app.set("view engine", "ejs");
+
+/*
+Tell the Express.js app to use body-parser for processing
+URL-encoded and JSON parameters.
+ */
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+app.use(express.json());
+
+//Static views
+//app.use(express.static("public"));
+
+// Route for the homepage
+app.get("/", (req, res) => {
+   res.render('index', {
+       title: 'Home Page'
+   });
+});
+
+// Route to add test, display test pages
+app.get('/displayTests', homeController.showTestCases);
+app.post('/addTest', homeController.addTestForm, homeController.showTestCases);
+app.get('/addTest', homeController.getNewTestCase);
+
+// Error controller routes
+// MUST BE PLACED AFTER ALL GET/POST
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+
+
+
+// Set application to listen at port 3000
+app.listen(app.get("port"), () => {
+    console.log(`Server running at http://localhost:${app.get("port")}`)
+});
